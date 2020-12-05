a78a837f29e407ab76098d70cdceb69de2ab343c
Joakim <joakimanderlind@gmail.com>
2020-12-05 12:38:03 +0100

    refactored 05 haskell

--- 05/Haskell/main.hs
+++ 05/Haskell/main.hs
@@ -1,25 +1,15 @@
-findRow :: String -> (Int,Int) -> Int
-findRow ('B':[]) (low,high) = high
-findRow ('F':[]) (low,high) = low
-findRow ('F':xs) (low,high) = findRow xs (low,high-(1+high-low) `div` 2)
-findRow ('B':xs) (low,high) = findRow xs (1+low+(high-low) `div` 2,high)
-
-
-findCol :: String -> (Int,Int) -> Int
-findCol ('R':[]) (low,high) = high
-findCol ('L':[]) (low,high) = low
-findCol ('L':xs) (low,high) = findCol xs (low,high-(1+high-low) `div` 2)
-findCol ('R':xs) (low,high) = findCol xs (1+low+(high-low) `div` 2,high)
-
 binaryIndex :: Char -> Char -> (String -> (Int,Int) -> Int)
 binaryIndex l h = runIndex
     where
        runIndex :: String -> (Int,Int) -> Int
-       runIndex (h:[]) (low,high) = high
-       runIndex (l:[]) (low,high) = low
-       runIndex (l:xs) (low,high) = runIndex xs (low,high-(1+high-low) `div` 2)
-       runIndex (h:xs) (low,high) = runIndex xs (1+low+(high-low) `div` 2,high)
- 
+       runIndex (c:[]) (low,high) | c == l    = low
+                                  | otherwise = high
+       runIndex (c:xs) (low,high) | c == l    = runIndex xs (low,high - diff - 1)
+                                  | otherwise = runIndex xs (low + diff + 1,high)
+            where diff = div (high-low) 2
+
+findRow = binaryIndex 'F' 'B'
+findCol = binaryIndex 'L' 'R'
 
 parseRowCol :: String -> (Int,Int)
 parseRowCol s = (findRow (take 7 s) (0,127), findCol (drop 7 s) (0,7))
