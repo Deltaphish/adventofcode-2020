cb719e9c8efcc34b77f749adc089b9aa672e0426
Joakim <joakimanderlind@gmail.com>
2020-12-06 09:19:23 +0100

    solved 06 p1 haskell

---
+++ 06/Haskell/main.hs
@@ -0,0 +1,22 @@
+import Data.List
+splitOnBlank :: String -> [String]
+splitOnBlank xs = splitOnBlank' xs ("",[""])
+
+splitOnBlank' :: String -> (String,[String]) -> [String]
+splitOnBlank' [] (rest,acc)     = reverse rest : acc
+splitOnBlank' ['\n'] (rest,acc) = reverse rest : acc
+splitOnBlank' [x]    (rest,acc) = reverse (x:rest) : acc 
+splitOnBlank' (s:x:xs) (rest,acc) | [s,x] == "\n\n" = splitOnBlank' xs ("",(reverse rest):acc)
+		                  | otherwise       = splitOnBlank' (x:xs) (s:rest,acc)
+
+countOC :: String -> Int
+countOC xs = length $ nub xs
+
+solve :: [String] -> Int
+solve xs = foldl (+) 0 $ map countOC xs
+
+readInput :: String -> IO [String]
+readInput fn = readFile fn >>= return.(map (filter ('\n'/=))). splitOnBlank
+
+main :: IO()
+main = readInput "../input" >>= print.solve
