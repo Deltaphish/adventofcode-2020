c2a0704cbfc7209115b145b2a14d1d66e2597f8e
Joakim <joakimanderlind@gmail.com>
2020-12-06 09:24:53 +0100

    solved 06 p2 haskell

--- 06/Haskell/main.hs
+++ 06/Haskell/main.hs
@@ -9,14 +9,15 @@ splitOnBlank' [x]    (rest,acc) = reverse (x:rest) : acc
 splitOnBlank' (s:x:xs) (rest,acc) | [s,x] == "\n\n" = splitOnBlank' xs ("",(reverse rest):acc)
 		                  | otherwise       = splitOnBlank' (x:xs) (s:rest,acc)
 
-countOC :: String -> Int
-countOC xs = length $ nub xs
+countOC :: [String] -> Int
+countOC [] = 0
+countOC xs = length $ nub $ foldl intersect (head xs) xs
 
-solve :: [String] -> Int
+solve :: [[String]] -> Int
 solve xs = foldl (+) 0 $ map countOC xs
 
-readInput :: String -> IO [String]
-readInput fn = readFile fn >>= return.(map (filter ('\n'/=))). splitOnBlank
+readInput :: String -> IO [[String]]
+readInput fn = readFile fn >>= return.(map lines). splitOnBlank
 
 main :: IO()
 main = readInput "../input" >>= print.solve
