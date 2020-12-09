f1a7659656a9f170d9ab9c2ae3204bde3eedcdde
Joakim <joakimanderlind@gmail.com>
2020-12-09 10:37:31 +0100

    Haskell 09 p2

--- 09/Haskell/day9/src/Lib.hs
+++ 09/Haskell/day9/src/Lib.hs
@@ -2,6 +2,7 @@ module Lib where
 
 import Data.Functor
 import Data.Maybe
+import Data.List
 
 import qualified Data.IntSet as IS
 import qualified Data.Sequence as S
@@ -12,6 +13,8 @@ type SumTables = S.Seq (Int,IS.IntSet)
 
 --(a -> b -> a) -> a -> [b] -> a
 --[Int,IS.IntSet] -> Int -> [Int, IS.IntSet]
+
+{- Find the outlier -}
 
 calcSums :: Seq (Int,IS.IntSet) -> Int -> Seq (Int,IS.IntSet)
 calcSums xs i = (i,set) <| xs
@@ -38,10 +41,43 @@ solver []     st  = Nothing
 solver (x:xs) st | checkInt st x = solver xs (addInt st x)
                  | otherwise     = Just x
 
-runSolver :: [Int] -> Int
-runSolver xs = fromJust $ solver xs' st
+findOutlier :: [Int] -> Int
+findOutlier xs = fromJust $ solver xs' st
     where xs' = drop 25 xs
           st = genInitPreable $ take 25 xs
+
+{- Find the sequence -}
+
+{-
+findseq:
+    starting with x,sum subsequent xs:
+        == -> FoundEM, return x and latest xs
+        >  -> move to x[+1], run again
+-}
+
+findSeq :: Int -> [Int] -> Maybe (Int,Int)
+findSeq target []          = Nothing
+findSeq target (x:xs) | target == x = Nothing
+                      | otherwise = case findSeq' xs x of
+                                        Nothing -> (findSeq target xs) 
+                                        Just a -> return a
+    where
+        findSeq' :: [Int] -> Int -> Maybe (Int,Int)
+        findSeq' [] acc                         = Nothing
+        findSeq' (t:ts) acc | t + acc == target = Just (x,t)
+                            | t + acc > target  = Nothing
+                            | otherwise         = findSeq' ts (t + acc)  
+
+
+findMinMax :: [Int] -> (Int,Int) -> Int
+findMinMax xs (low,high) = (maximum xs') + (minimum xs')
+    where
+        xs' = high : (takeWhile (/= high) $ dropWhile (/= low) xs)
+
+{-Glue-}
+
+runSolver :: [Int] -> Int
+runSolver xs = findMinMax xs $ fromJust $ findSeq (findOutlier xs) xs
 
 readInpt :: String -> IO [Int]
 readInpt f = readFile f <&> (map read).lines
