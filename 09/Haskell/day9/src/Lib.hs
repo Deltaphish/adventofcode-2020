8183c621e716019d2ece6ded67431ab5a55d0cb6
Joakim <joakimanderlind@gmail.com>
2020-12-09 12:38:47 +0100

    Revert "try global union"
    This reverts commit 927863adbe3c6d6acf51d157e79b22b4f573e6f7.

--- 09/Haskell/day9/src/Lib.hs
+++ 09/Haskell/day9/src/Lib.hs
@@ -8,37 +8,33 @@ import qualified Data.IntSet as IS
 import qualified Data.Sequence as S
 import Data.Sequence ((<|),(|>),Seq(..))
 
-data SumTables = SumTables (S.Seq (Int,IS.IntSet)) IS.IntSet deriving Show
+type SumTables = S.Seq (Int,IS.IntSet)
 
 
---(a -> b -> b) -> b -> [a] -> b
+--(a -> b -> a) -> a -> [b] -> a
 --[Int,IS.IntSet] -> Int -> [Int, IS.IntSet]
 
 {- Find the outlier -}
 
-seqToSet :: Seq Int -> IS.IntSet
-seqToSet = foldl (flip IS.insert) IS.empty
-
-calcSums :: Int -> Seq (Int,IS.IntSet) -> Seq (Int,IS.IntSet)
-calcSums i xs = (i,set) <| xs
+calcSums :: Seq (Int,IS.IntSet) -> Int -> Seq (Int,IS.IntSet)
+calcSums xs i = (i,set) <| xs
     where
         terms = fmap fst $ xs
         set = foldl (\a b -> IS.insert b a) IS.empty $ fmap (i+) terms
 
 genInitPreable :: [Int] -> SumTables
-genInitPreable xs = foldl calcSums S.empty xs
+genInitPreable = foldl calcSums S.empty
 
 checkInt :: SumTables -> Int -> Bool
-checkInt (SumTables xs global) x = IS.member x global
+checkInt xs x = IS.member x tables 
+    where
+        tables = IS.unions $ fmap snd xs
 
 addInt :: SumTables -> Int -> SumTables
-addInt (SumTables (old :<| xs) global) i = SumTables ( xs' |> (i,IS.empty)) global'
+addInt (_ :<| xs) i = fmap (addInt' i) xs |> (i,IS.empty)
     where
-        global' = IS.union (seqToSet newTerms) $  global `IS.difference`  (snd old)
-        (xs',newTerms) = S.unzip $ fmap (addInt' i) xs
-
-        addInt' :: Int -> (Int,IS.IntSet) -> ((Int,IS.IntSet),Int)
-        addInt' y (x,t) = ((x,IS.insert (x+y) t),(y+x))
+        addInt' :: Int -> (Int,IS.IntSet) -> (Int,IS.IntSet)
+        addInt' y (x,t) = (x,IS.insert (x+y) t)
 
 solver :: [Int] -> SumTables -> Maybe Int
 solver []     st  = Nothing 
@@ -48,7 +44,12 @@ solver (x:xs) st | checkInt st x = solver xs (addInt st x)
 findOutlier :: [Int] -> Int
 findOutlier xs = fromJust $ solver xs' st
     where xs' = drop 25 xs
-          st = genInitPreable $ take 25 xs
+          st  = genInitPreable $ take 25 xs
+          
+          solver :: [Int] -> SumTables -> Maybe Int
+          solver []     st                 = Nothing 
+          solver (x:xs) st | checkInt st x = solver xs (addInt st x)
+                           | otherwise     = Just x
 
 {- Find the sequence -}
 
