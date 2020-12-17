a25f8cb9af07190c907713a584ad523c2eadef3f
Joakim <joakimanderlind@gmail.com>
2020-12-17 11:01:33 +0100

    add doc

--- 01/SQL/adv01.sql
+++ 01/SQL/adv01.sql
@@ -1,3 +1,5 @@
+-- This script is only compatible with POSTGRES
+-- The "Solution" uses vanila sql, but \copy is used to read file into table
 DROP TABLE IF EXISTS advent01;
 CREATE TABLE advent01(
     expense INT PRIMARY KEY NOT NULL
@@ -5,6 +7,7 @@ CREATE TABLE advent01(
 
 \copy advent01 FROM '../input'
 
+\echo PART 1
 SELECT T1.expense, T2.expense, T1.expense * T2.expense as solution
     FROM
         advent01 as T1
@@ -14,6 +17,7 @@ SELECT T1.expense, T2.expense, T1.expense * T2.expense as solution
         T1.expense + T2.expense = 2020
     LIMIT 1;
 
+\echo PART 2
 SELECT T1.expense, T2.expense, T3.expense, T1.expense * T2.expense * T3.expense as solution
     FROM
         advent01 as T1
